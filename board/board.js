d296544c323c463d579b834ee5e093d2c852918e
Raul Ciucalau <ciucalauraul@gmail.com>
2025-05-13 20:49:45 +0200

    Board page: onclick add task

--- board/board.js
+++ board/board.js
@@ -3,3 +3,16 @@ function init() {
     includeHTML();
 }
 
+function openAddTaskDialog() {
+    let dialog = document.getElementById('addTaskDialog');
+    dialog.classList.toggle('d-none');
+}
+
+function toggleDialog() {
+    let dialog = document.getElementById('addTaskDialog')
+    dialog.classList.toggle('d-none');
+}
+
+function stopPropagation(event) {
+    event.stopPropagation();
+}
