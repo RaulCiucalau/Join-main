24569623a18e46dada661f2d7f6260306c07006e
Raul Ciucalau <ciucalauraul@gmail.com>
2025-05-23 13:32:01 +0200

    Board:  edit task dialog - due date  field & priority field - CSS & Js

--- board/dialog-edit-task.js
+++ board/dialog-edit-task.js
@@ -85,3 +85,40 @@ function removeUnSelectedAvatar(i) {
   const el = document.getElementById(`avatar-${i}`);
   if (el) el.remove();
 }
+
+function selectPrio(prio) {
+  const prios = ["urgent", "medium", "low"];
+  prios.forEach((p) => unselectPrio(p));
+  document.getElementById(`prio-img-${prio}`).src = `../assets/icons/priority-${prio}-white.svg`;
+  document.getElementById(`prio-btn-${prio}`).style.backgroundColor = getPrioColor(prio);
+  document.getElementById(`prio-btn-${prio}`).style.color = "white";
+  selectedPrio = `${prio}`;
+}
+
+function unselectPrio(prio) {
+  document.getElementById(`prio-img-${prio}`).src = `../assets/icons/priority-${prio}.svg`;
+  document.getElementById(`prio-btn-${prio}`).style.backgroundColor = "white";
+  document.getElementById(`prio-btn-${prio}`).style.color = "black";
+  selectedPrio = "";
+}
+
+function togglePriority(prio) {
+  if (document.getElementById(`prio-btn-${prio}`).style.color === "white") {
+    unselectPrio(prio);
+  } else {
+    selectPrio(prio);
+  }
+}
+
+function getPrioColor(prio) {
+  switch (prio) {
+    case "urgent":
+      return "#FF3D00";
+    case "medium":
+      return "#FFA800";
+    case "low":
+      return "#7AE229";
+    default:
+      return "white";
+  }
+}
