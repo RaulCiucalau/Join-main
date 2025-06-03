35e43773787fdd2c4c2c3e7fef6c0296915cdff7
StefanSchad <stefanschad46@gmail.com>
2025-06-03 22:15:56 +0200

    JoinSign animation

--- js/login.js
+++ js/login.js
@@ -1,3 +1,14 @@
+function init(){
+
+    // animationJoinSign()
+}
+
 function redirectToGuestView() {
     location.replace("summary/summary.html");
+}
+
+function animationJoinSign(){
+    let animationJoinSign = document.getElementById('overlay-animation');
+
+    animationJoinSign.classList.toggle('d_none');
 }
