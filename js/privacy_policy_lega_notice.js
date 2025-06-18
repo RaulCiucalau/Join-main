acde0c5267fff515b584749105a4f7b43f79e8a1
Raul Ciucalau <ciucalauraul@gmail.com>
2025-06-18 20:47:17 +0200

    help, privacy policy, legal  - fix avatar for header

--- js/privacy_policy_lega_notice.js
+++ js/privacy_policy_lega_notice.js
@@ -1,3 +1,15 @@
 function init() {
     includeHTML();
 }
+
+function waitForInitialLetterElement(callback) {
+  const observer = new MutationObserver(() => {
+    const el = document.getElementById("initialLetter");
+    if (el) {
+      observer.disconnect();
+      callback();
+    }
+  });
+
+  observer.observe(document.body, { childList: true, subtree: true });
+}
